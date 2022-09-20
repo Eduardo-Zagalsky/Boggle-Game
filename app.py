74e9eb603115dd7b4b3a7c1e5aac33f991571963
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-09-20 15:00:21 -0500

    added make board route and session

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from boggle import Boggle
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "keep-it-secret"
 debug = DebugToolbarExtension(app)
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 boggle_game = Boggle()
 
 @app.route("/")
 def root():
     return render_template("home.html")
+
+@app.route("/board")
+def make_board():
+    board = boggle_game.make_board()
+    session["board"] = board
+
+    return render_template("board.html", board = board)
