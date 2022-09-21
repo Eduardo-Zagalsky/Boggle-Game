65eaa667f0a58cdee69765a44872e18334890e59
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-09-21 18:33:36 -0500

    added python formatter to make it look better, added logic for checking word and jsonified it

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 from boggle import Boggle
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "keep-it-secret"
+app.config["SECRET_KEY"] = "keep-it-secret"
 debug = DebugToolbarExtension(app)
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 boggle_game = Boggle()
 
+
 @app.route("/")
 def root():
     return render_template("home.html")
+
 
 @app.route("/board")
 def make_board():
     board = boggle_game.make_board()
     session["board"] = board
 
-    return render_template("board.html", board = board)
+    return render_template("board.html", board=board)
+
+
+@app.route("/check_word")
+def check_valid_word():
+    word = request.args["guess"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+    return jsonify({"result": response})
