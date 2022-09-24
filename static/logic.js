206ef94f429588724f5341f5073252ecc8050748
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-09-23 21:32:33 -0500

    use axios to send the word to the server to validate it, receive it back and decide what to do with the word

--- static/logic.js
+++ static/logic.js
@@ -1,8 +1,25 @@
+const ul = document.querySelector("ul");
 let gbutton = document.getElementById("guess-button");
 gbutton.addEventListener("click", async function (event) {
     event.preventDefault();
-    let guess = document.getElementById("guess").value;
-    const sendGuess = await axios.get("/check_word", { params: { word: guess } });
-    let response = sendGuess.result;
-    // if(response == "")
+    let input = document.getElementById("word");
+    let word = input.value;
+    const sendGuess = await axios.get("/check_word", { params: { word: word } });
+    let response = sendGuess.data.result;
+    if (response == "not-on-board") {
+        alert("Word not on Board");
+    } else if (response == "not-word") {
+        alert("Not a Real Word");
+    } else if (response == "ok") {
+        let li = document.createElement("li");
+        li.innerText = word.toUpperCase();
+        input.value = '';
+        ul.append(li);
+    }
+
 });
+const allTd = document.querySelectorAll("td");
+
+
+// click letters to highlight and add table for words that have already been used
+// delete box when guess is done
